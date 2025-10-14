3f58300f632b083550cdf2d1064dc6a4b99c1cc7
Gernot Steinegger <Gernot.Steinegger@roomle.com>
2025-10-14 14:37:14 +0200

    fix: update dependencies and HI setup

--- nobilia-web-sdk-demo/src/App.vue
+++ nobilia-web-sdk-demo/src/App.vue
@@ -7,13 +7,135 @@ import RoomleEmbeddingApi from "@roomle/embedding-lib";
 import apiOptions from './utils/default-api-options';
 import {onMounted} from "vue";
 import {calculateTotalSum, getQueryParam} from "./utils/helpers";
-import {loadArticleCatalog, loadCalcScript, loadMasterData} from "./utils/loader.ts";
+// @ts-ignore
+import {createExtObjId} from "@roomle/web-sdk";
 
 // Data
 
 const FAKE_ROOT_TAG = 'external:root-tag';
 
 // Methods
+
+const HOMAG_INTELLIGENCE_ENDPOINT =
+  'https://europe-west3-rml-showcases.cloudfunctions.net/proxy_request?url=';
+
+interface HomagIntelligenceInitData {
+  libraryId: string;
+  serverOptions?: ApiOptions;
+  debugLogging?: boolean;
+}
+
+interface OrderManagerOptions {
+  key?: string;
+  importBaseUrl?: string;
+}
+
+interface ApiOptions {
+  subscriptionId: string;
+  om?: OrderManagerOptions;
+  endpointUrl?: string;
+  localUrl?: string;
+}
+
+const fetchDataWithAuthorization = async (
+  url: string,
+  type: 'json' | 'text',
+  apiOptions: ApiOptions,
+  debug: { property: string } = { property: 'default' },
+) => {
+  const authorizationHeaders = {
+    headers: {
+      'Content-Type': type === 'json' ? 'application/json' : 'text/plain',
+      'Access-Control-Allow-Origin': '*',
+    },
+  };
+  try {
+    const startTime = performance.now();
+    const { subscriptionId, endpointUrl } = apiOptions;
+    const key = ''; //om?.key || '';
+    const response = await fetch(
+      HOMAG_INTELLIGENCE_ENDPOINT +
+        encodeURIComponent(url) +
+        `&subscriptionId=${encodeURIComponent(subscriptionId)}` +
+        (key ? `&apiKey=${encodeURIComponent(key)}` : '') +
+        (endpointUrl ? `&baseUrl=${encodeURIComponent(endpointUrl)}` : ''),
+      authorizationHeaders,
+    );
+    const endTime = performance.now();
+    const fetchTime = endTime - startTime; // Calculate the fetch time
+
+    if (!response.ok) {
+      console.warn(`Failed to fetch data from ${debug.property}`);
+      throw new Error(
+        `Failed to fetch data from ${url}: ${response.statusText}`,
+      );
+    }
+    console.info(`Success to fetch data from ${debug.property}`);
+    console.info(
+      `The data for ${debug.property} was fetched in ${fetchTime} milliseconds`,
+    );
+
+    return response;
+  } catch (error) {
+    console.warn(`Failed to fetch data from ${debug.property}`);
+    console.error(error);
+    throw error;
+  }
+};
+
+const loadArticleCatalog = async (
+  apiOptions: HomagIntelligenceInitData,
+) => {
+  const { libraryId, serverOptions } = apiOptions;
+  // NOBILIA DEMO HACKS
+  const endpoint = libraryId.includes('nobilia')
+    ? `api/pos/libraries/${libraryId}/articles`
+    : `api/pos/articles?libraryId=${libraryId}`;
+  const articlesCatalog = await fetchDataWithAuthorization(
+    endpoint,
+    'json',
+    serverOptions as ApiOptions,
+    { property: 'articles' },
+  );
+  return await articlesCatalog.json();
+};
+
+const loadMasterData = async (
+  apiOptions: HomagIntelligenceInitData,
+) => {
+  const libraryId = apiOptions.libraryId;
+  const serverOptions = apiOptions.serverOptions as ApiOptions;
+  // NOBILIA DEMO HACKS
+  const endpoint = libraryId.includes('nobilia')
+    ? `api/pos/libraries/${libraryId}/masterData`
+    : `api/pos/masterData?libraryId=${libraryId}`;
+  const masterData = await fetchDataWithAuthorization(
+    endpoint,
+    'json',
+    serverOptions,
+    { property: 'masterData' },
+  );
+  return (await masterData.json()) as any;
+};
+
+const loadCalcScript = async (
+  apiOptions: HomagIntelligenceInitData,
+) => {
+  const libraryId = apiOptions.libraryId;
+  const serverOptions = apiOptions.serverOptions as ApiOptions;
+  // NOBILIA DEMO HACKS
+  const url = libraryId.includes('nobilia')
+    ? `api/pos/libraries/${libraryId}/calc.js`
+    : 'api/pos/calc.js?libraryId=' + encodeURIComponent(libraryId);
+  const response = await fetchDataWithAuthorization(url, 'text', serverOptions, {
+    property: 'calc.js',
+  });
+  let jsCode = await response.text();
+  if (!jsCode) {
+    throw new Error('Script load error');
+  }
+  return jsCode;
+};
 
 const onRequestPlan = (roomDesignerApi: any) => {
   roomDesignerApi.extended.sendToOM(true);
@@ -55,16 +177,14 @@ const startRoomlePlanner = async () => {
       'load-idb-config': false,
       'stop-configure-hint': false,
     },
-    overrideServerUrl: 'https://roomle.com/t/cp-internal',
-    catalogRootTag: `__ext__obj__#${FAKE_ROOT_TAG}`,
+    overrideServerUrl: 'https://www.roomle.com/t/bo-test',
+    catalogRootTag: createExtObjId(FAKE_ROOT_TAG),
     featureFlags: {
       globalCallbacks: false,
       openCloseAnimation: true,
       slopingRoof: true,
     },
     autoStart: true,
-    restrictionLevel: 1,
-    deeplink: 'https://roomle-dev.github.io/nobilia-web-sdk-demo?plan_id=#CONFIGURATIONID#  ',
     hi: {
       libraryId: apiOptions.tecConfigInfo.libraryId,
       serverOptions: {
@@ -83,12 +203,9 @@ const startRoomlePlanner = async () => {
   }
 
   RoomleEmbeddingApi.setupHi({
-    onLoadJavascript: (libraryId: string) =>
-        loadCalcScript(libraryId, apiOptions.tecConfigInfo),
-    onLoadArticleCatalog: (libraryId: string) =>
-        loadArticleCatalog(libraryId, apiOptions.tecConfigInfo),
-    onLoadMasterData: (libraryId: string) =>
-        loadMasterData(libraryId, apiOptions.tecConfigInfo),
+    onLoadJavascript: (_libraryId) => loadCalcScript(options.hi),
+    onLoadArticleCatalog: (_libraryId) => loadArticleCatalog(options.hi),
+    onLoadMasterData: (_libraryId) => loadMasterData(options.hi),
   });
 
   const instance = await RoomleEmbeddingApi.createPlanner(
@@ -96,6 +213,8 @@ const startRoomlePlanner = async () => {
       document.getElementById('container')!,
       options,
   );
+
+  await instance.ui.loadObject(planId);
 
   instance.extended.callbacks.onCompletelyLoaded = () => {
     doPriceCalculation(instance);
@@ -114,13 +233,11 @@ const startRoomlePlanner = async () => {
   };
 
   (window as any).instance = instance;
-
-  await instance.ui.loadObject(planId);
 }
 
 // Hooks
 
-onMounted(async () => await startRoomlePlanner())
+onMounted(() => startRoomlePlanner())
 </script>
 
 <style scoped>
